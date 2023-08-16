6906cf733e4ae850203d90ff82ddb167ab1fe415
Claudio Castro <claudio.87_@hotmail.com>
2023-08-16 16:25:39 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let numeroAzar = Math.floor(Math.random() * 100) + 1;
+let numeroEntrada = document.getElementById('numeroEntrada');
+let mensaje = document.getElementById('mensaje');
+
+
+function chequearResultado(){
+
+}
